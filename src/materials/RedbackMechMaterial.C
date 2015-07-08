99cca1c205a450c3477a8cbb5d944bdcca4f34c9
Thomas Poulet <thomas.poulet@csiro.au>
2015-07-08 16:44:38 +1000

    new formula

--- src/materials/RedbackMechMaterial.C
+++ src/materials/RedbackMechMaterial.C
@@ -508,7 +508,10 @@ RedbackMechMaterial::returnMap(const RankTwoTensor & sig_old, const RankTwoTenso
   }
 
   //The following expression should be further pursued for a forward physics-based model
-  _exponential = _exponential* std::exp(-_alpha_1[_qp]*_confining_pressure[_qp] - _pore_pres[_qp]*_alpha_2[_qp]*(1 + _alpha_3[_qp]*std::log(_confining_pressure[_qp])));
+  //_exponential = _exponential* std::exp(-_alpha_1[_qp]*_confining_pressure[_qp] - _pore_pres[_qp]*_alpha_2[_qp]*(1 + _alpha_3[_qp]*std::log(_confining_pressure[_qp])));
+  Real V_act = 1 + _alpha_3[_qp]*std::log(_confining_pressure[_qp]);
+  Real P_act = -_alpha_1[_qp]*_confining_pressure[_qp] + _alpha_2[_qp]*_pore_pres[_qp];
+  _exponential = _exponential* std::exp(-P_act*V_act);
 
   while (err3 > tol3 && iterisohard < maxiterisohard) //Hardness update iteration
   {
