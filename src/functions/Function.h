4df596e51d3b981d44256c28c13964cac2cb6f5b
RebeccaTung <rebecca.tung@postgrad.curtin.edu.au>
2015-11-05 11:29:47 +0800

    Copied ConstantFunction.C and .h, and Function.h from MOOSE functions

---
+++ src/functions/Function.h
@@ -0,0 +1,98 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+#include "MooseObject.h"
+#include "SetupInterface.h"
+#include "TransientInterface.h"
+#include "PostprocessorInterface.h"
+#include "UserObjectInterface.h"
+#include "Restartable.h"
+#include "MeshChangedInterface.h"
+#include "ScalarCoupleable.h"
+
+// libMesh
+#include "libmesh/vector_value.h"
+#include "libmesh/point.h"
+
+class Function;
+
+template<>
+InputParameters validParams<Function>();
+
+/**
+ * Base class for function objects.  Functions override value to supply a
+ * value at a point.
+ */
+class Function :
+  public MooseObject,
+  public SetupInterface,
+  public TransientInterface,
+  public PostprocessorInterface,
+  public UserObjectInterface,
+  public Restartable,
+  public MeshChangedInterface,
+  public ScalarCoupleable
+{
+public:
+  /**
+   * Class constructor
+   * \param name The name of the function
+   * \param parameters The input parameters for the function
+   */
+  Function(const std::string & name, InputParameters parameters);
+
+  /**
+   * Function destructor
+   */
+  virtual ~Function();
+
+  /**
+   * Override this to evaluate the scalar function at point (t,x,y,z), by default
+   * this returns zero, you must override it.
+   * \param t The time
+   * \param p The Point in space (x,y,z)
+   * \return A scalar of the function evaluated at the time and location
+   */
+  virtual Real value(Real t, const Point & p);
+
+  /**
+   * Override this to evaluate the vector function at a point (t,x,y,z), by default
+   * this returns a zero vector, you must override it.
+   * \param t The time
+   * \param p The Point in space (x,y,z)
+   * \return A vector of the function evaluated at the time and location
+   */
+  virtual RealVectorValue vectorValue(Real t, const Point & p);
+
+  /**
+   * Function objects can optionally provide a gradient at a point. By default
+   * this returns 0, you must override it.
+   * \param t The time
+   * \param p The Point in space (x,y,z)
+   * \return A gradient of the function evaluated at the time and location
+   */
+  virtual RealGradient gradient(Real t, const Point & p);
+
+  // Not defined
+  virtual Real integral();
+
+  // Not defined
+  virtual Real average();
+};
+
+#endif //FUNCTION_H
+
