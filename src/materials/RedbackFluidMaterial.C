da40f76247e7a1e9bce8322269ae4bb3437378eb
Thomas Poulet <Thomas.Poulet@csiro.au>
2015-11-13 11:50:08 +1100

    Updated the convective terms of the mass balance equation

--- src/materials/RedbackFluidMaterial.C
+++ src/materials/RedbackFluidMaterial.C
@@ -129,7 +129,7 @@ RedbackFluidMaterial::computeRedbackTerms()
   // Assembling mass kernels components
   _pressurization_coefficient[_qp] = _fluid_thermal_expansion_param/_fluid_compressibility_param;
   RealVectorValue fluid_vel_vector = RealVectorValue(_fluid_vel_x[_qp], _fluid_vel_y[_qp], _fluid_vel_z[_qp]);
-  _pressure_convective_mass[_qp] = _grad_pore_pressure[_qp]*fluid_vel_vector;
+  _pressure_convective_mass[_qp] = _grad_pore_pressure[_qp]*fluid_vel_vector - _pressurization_coefficient[_qp]*_grad_temp[_qp]*fluid_vel_vector;
   _thermal_convective_mass[_qp] = _grad_temp[_qp]*fluid_vel_vector;
 
   return;
