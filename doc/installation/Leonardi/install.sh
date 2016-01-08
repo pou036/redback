c8e2245c9270c4541d1c6fe36089ff6b4920ce5e
Jack Lin <jack_lin@zoho.com>
2016-01-08 14:41:16 +1100

    #30 deleted unneeded module; cleaning up

--- doc/installation/Leonardi/install.sh
+++ doc/installation/Leonardi/install.sh
@@ -8,19 +8,22 @@ DO_COMPILE_LIBMESH=1 # value 0 or 1. No space around the equal sign!  This al
 DO_INSTALL_OR_UPDATE_MOOSE_REDBACK=1 # value 0 or 1. No space around the equal sign!
 
 
+####### Nothing to touch below ! #############################
+export INSTALL_ROOT_DIR=~
+
 #setting up environment variables
 echo "starting:" `date`
 export NUM_PROC=12
 export MOOSE_JOBS=$NUM_PROC
 
-mkdir ~/moose-compilers
-export PACKAGES_DIR=~/moose-compilers
+mkdir $INSTALL_ROOT_DIR/moose-compilers
+export PACKAGES_DIR=$INSTALL_ROOT_DIR/moose-compilers
 
-mkdir ~/moose-compilers/modulefiles
+mkdir $INSTALL_ROOT_DIR/moose-compilers/modulefiles
 export MODULEPATH=$MODULEPATH:$PACKAGES_DIR/modulefiles
 
 #copy the module file over if it doesn't already exist
-cp ~/moose-dev-gcc ~/moose-compilers/modulefiles
+cp ~/moose-dev-gcc $INSTALL_ROOT_DIR/moose-compilers/modulefiles
 
 # loading modules
 #these two are preexisting
@@ -30,15 +33,25 @@ module load opt-python
 # this one is in $PACKAGES_DIR/modulefiles
 module load moose-dev-gcc
 
+#deprecated
+#source ~/.moose-profile
 
-source ~/.moose-profile
+set base_path   $INSTALL_ROOT_DIR/moose-compilers
+export CC=mpicc
+export CXX=mpicxx
+export F90=mpif90
+export F77=mpif77
+export FC=mpif90
+export ARCH=gcc
+export PETSC_DIR=$PACKAGES_DIR/petsc/petsc-3.6.0/$ARCH-opt
+
 if [ $DO_INSTALL_PREREQUISITES -eq 1 ]
   then
     printf "\n\n**** step 1: petsc ****\n\n"
 
     # petsc
-    mkdir ~/tmp
-    export CLUSTER_TEMP=`mktemp -d ~/tmp/cluster_temp.XXXXXX`
+    mkdir $INSTALL_ROOT_DIR/tmp
+    export CLUSTER_TEMP=`mktemp -d $INSTALL_ROOT_DIR/tmp/cluster_temp.XXXXXX`
     cd $CLUSTER_TEMP
     curl -L -O http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-3.6.0.tar.gz
     umask 0022
@@ -75,8 +88,8 @@ if [ $DO_INSTALL_PREREQUISITES -eq 1 ]
 
 
     #TODO: are these actually necessary? seem to be just benchmarks
-    #make PETSC_DIR=~/moose-compilers/petsc/petsc-3.6.0/gcc-opt PETSC_ARCH="" test
-    #make PETSC_DIR=/home/z3465651/moose-compilers/petsc/petsc-3.6.0/gcc-opt PETSC_ARCH= streams NPMAX=$NUM_PROC
+    #make PETSC_DIR=$INSTALL_ROOT_DIR/moose-compilers/petsc/petsc-3.6.0/gcc-opt PETSC_ARCH="" test
+    #make PETSC_DIR=$INSTALL_ROOT_DIR/moose-compilers/petsc/petsc-3.6.0/gcc-opt PETSC_ARCH= streams NPMAX=$NUM_PROC
 
 
     #cleaning up
@@ -90,18 +103,18 @@ if [ $DO_COMPILE_LIBMESH -eq 1 ] || [ $DO_INSTALL_OR_UPDATE_MOOSE_REDBACK -eq 1 
 
 
     #setting up Moose
-    mkdir ~/projects
-    cd ~/projects
+    mkdir $INSTALL_ROOT_DIR/projects
+    cd $INSTALL_ROOT_DIR/projects
     git clone https://github.com/idaholab/moose.git
-    cd ~/projects/moose
+    cd $INSTALL_ROOT_DIR/projects/moose
     git checkout master
     git pull
     #here we force the libmesh configure file to use PAX in single-volume mode.
     #https://lists.gnu.org/archive/html/automake/2011-03/msg00113.html
     #sed is different on osx and linux.  this is the osx version:
-    #sed -i '' "s/'pax -r'/'pax -O -r'/g" ~/projects/moose/libmesh/configure
+    #sed -i '' "s/'pax -r'/'pax -O -r'/g" $INSTALL_ROOT_DIR/projects/moose/libmesh/configure
     #and this is the linux version:
-    sed -i -e 's/pax -r/pax -O -r/g' ~/projects/moose/libmesh/configure
+    sed -i -e 's/pax -r/pax -O -r/g' $INSTALL_ROOT_DIR/projects/moose/libmesh/configure
 
 fi # end of step 2
 
@@ -117,16 +130,16 @@ fi
 if [ $DO_INSTALL_OR_UPDATE_MOOSE_REDBACK -eq 1 ]
   then
     printf "\n\n**** step 4: moose build and tests ****\n\n"
-    cd ~/projects/moose/test
+    cd $INSTALL_ROOT_DIR/projects/moose/test
     make -j$NUM_PROC
     ./run_tests -j$NUM_PROC
     #TODO: pipe the results; make sure it says "0 failed"
 
     printf "\n\n**** step 5: redback ****\n\n"
     #setting up Redback
-    cd ~/projects
+    cd $INSTALL_ROOT_DIR/projects
     git clone https://github.com/pou036/redback
-    cd ~/projects/redback
+    cd $INSTALL_ROOT_DIR/projects/redback
     git checkout master
     git pull
     make -j$NUM_PROC
