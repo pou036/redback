dd87b44f923ccd16184e1838f8b2e1f8d07ba7bc
Thomas Poulet <Thomas.Poulet@csiro.au>
2016-05-17 11:06:24 +1000

    Added optional min and max bounds

--- src/ics/FunctionNormalDistributionIC.C
+++ src/ics/FunctionNormalDistributionIC.C
@@ -24,20 +24,27 @@ validParams<FunctionNormalDistributionIC>()
   params.addRequiredParam<Real>("mean", "Mean value of normal distribution function");
   params.addRequiredParam<Real>("standard_deviation", "Standard deviation of normal distribution function");
   params.addParam<unsigned int>("seed", 0, "Seed value for the random number generator");
+  params.addParam<Real>("minimum", -1e99, "minimum value (if you want to cap the range of available values)");
+  params.addParam<Real>("maximum", 1e99, "minimum value (if you want to cap the range of available values)");
   return params;
 }
 
 FunctionNormalDistributionIC::FunctionNormalDistributionIC(const InputParameters & parameters) :
     InitialCondition(parameters),
     _mean(getParam<Real>("mean")),
-    _stddev(getParam<Real>("standard_deviation"))
+    _stddev(getParam<Real>("standard_deviation")),
+    _min(getParam<Real>("minimum")),
+    _max(getParam<Real>("maximum"))
 {
   mooseAssert(_stddev > 0.0, "standard_deviation must be positive for FunctionNormalDistributionIC!");
+  Real range = _max - _min;
+  mooseAssert(range >= 0.0, "The maximum value must be greater than the minimum in FunctionNormalDistributionIC");
   MooseRandom::seed(getParam<unsigned int>("seed"));
 }
 
 Real
 FunctionNormalDistributionIC::value(const Point & p)
 {
-  return MooseRandom::randNormal(_mean, _stddev);
+  Real result = MooseRandom::randNormal(_mean, _stddev);
+  return fmax(_min, fmin(_max, result));
 }
