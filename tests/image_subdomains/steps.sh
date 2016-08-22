e1da80eb3a1f6c1bb6c78a4cc81312e8b7bdb08f
Martin Lesueur <martin@Martin.local>
2016-08-22 12:27:32 +1000

    #43 Update of the process to mesh ct-scans Test files updated in consequence

--- tests/image_subdomains/steps.sh
+++ tests/image_subdomains/steps.sh
@@ -2,10 +2,11 @@
 
 ### script that runs all steps for creating a mesh from a Ct scan image
 
-~/projects/redback/redback-opt -i mesh_generation_step1.i > result.txt
-echo  end of first step, created adapted 1block mesh
-~/projects/redback/redback-opt -i mesh_generation_step2.i >> result.txt
-echo  end of second step, created file "idfile.txt"
+echo  'Start of first step on' `date`
+~/projects/redback/redback-opt -i mesh_generation_step1.i -j $MOOSE_JOBS > result.txt
+echo  'End of first step, created adapted 1block mesh on' `date`
+~/projects/redback/redback-opt -i mesh_generation_step2.i -j $MOOSE_JOBS >> result.txt
+echo  'End of second step, created file "idfile.txt" on' `date`
 python remove_duplicates.py
-~/projects/redback/redback-opt -i mesh_generation_step3.i >> result.txt
-echo  end of last step, created adapted 2blocks mesh
+~/projects/redback/redback-opt -i mesh_generation_step3.i -j $MOOSE_JOBS >> result.txt
+echo  'End of last step, created adapted 2blocks mesh on' `date`
