3b0e6d97ff20613bf78169ef573a5148cb72c992
Martin Lesueur <martin@Martin.local>
2018-03-02 09:08:08 +1100

    #19 corrected the implementation of computing the tangential stress to the fault

--- src/auxkernels/RankTwoProjectionAux.C
+++ src/auxkernels/RankTwoProjectionAux.C
@@ -14,7 +14,7 @@ validParams<RankTwoProjectionAux>()
   InputParameters params = validParams<AuxKernel>();
   params.addClassDescription("Compute the norm of a RankTwoTensor projected on a vector");
   params.addRequiredParam<MaterialPropertyName>("rank_two_tensor", "The rank two material tensor name");
-  params.addRequiredParam<Real>("fault_angle", "The angle of the fault in degrees");
+  params.addRequiredParam<Real>("fault_angle", "The angle of the fault with respect to the horizontal in degrees");
   params.addParam<bool>("normal",
                         false,
                         "If true, the normal value to the fault "
@@ -22,8 +22,7 @@ validParams<RankTwoProjectionAux>()
   params.addParam<bool>("convert_to_radians",
                         false,
                         "If true, the value you entered will be "
-                        "multiplied by Pi/180 before passing to the "
-                        "Plasticity algorithms");
+                        "multiplied by Pi/180");
   return params;
 }
 
@@ -39,9 +38,17 @@ RankTwoProjectionAux::RankTwoProjectionAux(const InputParameters & parameters) :
 Real
 RankTwoProjectionAux::computeValue()
 {
-  RealVectorValue fault(-sin(_angle), cos(_angle));
-  if (_normal)
-    fault = RealVectorValue(cos(_angle), sin(_angle));
-  RealVectorValue vec_temp(_tensor[ _qp ].row(0) * fault, _tensor[ _qp ].row(1) * fault);
-  return std::pow(std::pow(vec_temp(0), 2) + std::pow(vec_temp(1), 2), 0.5);
+  // traction ti = sigma_ij . n_i
+  // ti = ti_t + ti_n
+  // ||ti_t|| = (ti - ||ti_n||.n_i) . t_i
+  // ||ti_t|| = (ti - (ti . n_i).n_i) . t_i
+  RealVectorValue fault_normal(-sin(_angle), cos(_angle));
+  RealVectorValue fault_tangent(cos(_angle), sin(_angle));
+  RealVectorValue traction = _tensor[ _qp ] * fault_normal;
+  return (traction - (traction * fault_normal) * fault_normal) * fault_tangent;
+  // RealVectorValue fault(-sin(_angle), cos(_angle));
+  // if (_normal)
+  //   fault = RealVectorValue(cos(_angle), sin(_angle));
+  // RealVectorValue vec_temp(_tensor[ _qp ].row(0) * fault, _tensor[ _qp ].row(1) * fault);
+  // return std::pow(std::pow(vec_temp(0), 2) + std::pow(vec_temp(1), 2), 0.5);
 }
